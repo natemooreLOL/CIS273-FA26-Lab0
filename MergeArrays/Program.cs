c8d8d0480fcec7c7372e52755e2a6dc77d03615e
Nate Moore <nate.moore@students.fhu.edu>
2026-09-18 23:47:30 -0500

    mergearrays and vector struct

--- MergeArrays/Program.cs
+++ MergeArrays/Program.cs
@@ -10,19 +10,82 @@ public class Program
     // TODO 
     public static int[] MergeSortedArrays(int[] array1, int[] array2)
     {
-        return null;
+        int[] SortedArray = [];
+        int Array1Index = 0;
+        int Array2Index = 0;
+        while (Array1Index < array1.Length || Array2Index < array2.Length)
+        {
+            if (Array1Index == array1.Length)
+            {
+                SortedArray.Append(array2[Array2Index]);
+                Array2Index++;
+            }
+            if(Array2Index == array2.Length)
+            {
+                SortedArray.Append(array1[Array1Index]);
+                Array1Index++;
+            }
+            if(array1[Array1Index] > array2[Array2Index])
+            {
+                SortedArray.Append(array1[Array1Index]);
+                Array1Index++;
+            }
+            else
+            {
+                SortedArray.Append(array2[Array2Index]);
+                Array2Index++;
+            }
+        }
+        return SortedArray;
     }
 
-    // TODO 
+
     private static bool IsSorted(int[] array)
     {
+        if (array.Length == 0)
+        {
+            return true;
+        }
+        for (int i=1; i<array.Length; i++)
+        {
+            if (array[i - 1] > array[i])
+            {
+                return false;
+            }
+        }
         return true;
     }
 
     // TODO
     public static T[] MergeSortedArrays<T>(T[] array1, T[] array2) where T:IComparable<T>
     {
-
+        T[] SortedArray = [];
+        int Array1Index = 0;
+        int Array2Index = 0;
+        while (Array1Index < array1.Length || Array2Index < array2.Length)
+        {
+            if (Array1Index == array1.Length)
+            {
+                SortedArray.Append(array2[Array2Index]);
+                Array2Index++;
+            }
+            if(Array2Index == array2.Length)
+            {
+                SortedArray.Append(array1[Array1Index]);
+                Array1Index++;
+            }
+            if(array1[Array1Index].CompareTo(array2[Array2Index]) > 0)
+            {
+                SortedArray.Append(array1[Array1Index]);
+                Array1Index++;
+            }
+            else
+            {
+                SortedArray.Append(array2[Array2Index]);
+                Array2Index++;
+            }
+        }
+        return SortedArray;
         return default;     
     }
 }
--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -1,6 +1,25 @@
-﻿namespace Vector;
+﻿using System.Runtime.CompilerServices;
+using System;
+namespace Vector;
 
 public struct Vector
 {
     
+    public double Magnitude;
+
+
+    public double X;
+
+    public double Y;
+
+    public double Direction;
+
+    public Vector(double x, double y)
+    {
+        X = x;
+        Y = y;
+        Direction=Math.Atan2(Y,X)*180/Math.PI;
+        Magnitude=Math.Sqrt(x*x+y*y);
+    }
+
 }
